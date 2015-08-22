26e882d12c31c3349c441451304d3d1777368e30
João Pedro Perondini <jpfperondini@gmail.com>
2015-08-22 17:08:53 -0300

    add docker-compose

--- init.sh
+++ init.sh
@@ -56,4 +56,8 @@ cd ~
 curl -sSL https://get.docker.com/ | sh
 sudo usermod -aG docker $(whoami)
 
-
+#docker compose
+sudo -i
+curl -L https://github.com/docker/compose/releases/download/1.4.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+exit
