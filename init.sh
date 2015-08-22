31b59df5261d1b6d074e7fe4873d69c575dab308
João Pedro Perondini <jpfperondini@gmail.com>
2015-08-22 16:52:27 -0300

    add some tools install script

---
+++ init.sh
@@ -0,0 +1,56 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
+cd ~
+
+#Directories
+mkdir ~/projects
+mkdir ~/tools
+
+#PPA Repositories
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+sudo add-apt-repository ppa:webupd8team/brackets
+sudo add-apt-repository ppa:webupd8team/java
+
+#Update
+sudo apt-get update
+
+#PPA Programs
+sudo apt-get install oracle-java8-installer vim google-chrome-stable brackets curl hub
+
+#Intellij
+mkdir -p ~/tools/intellij/14/
+cd ~/tools/intellij/14/
+wget https://download.jetbrains.com/idea/ideaIC-14.1.4.tar.gz
+tar xfz ideaIC-14.1.4.tar.gz
+mv idea-IC-141.1532.4/* .
+rm ideaIC-14.1.4.tar.gz
+rm -r idea-IC-141.1532.4
+#TODO add intellij to path?
+cd ~
+
+#docker
+#Disable this line if you are behind a proxy.
+#curl -sSL https://get.docker.com/gpg | sudo apt-key add -
+mkdir -p ~/tools/docker
+cd ~/tools/docker
+curl -sSL https://get.docker.com/ | sh
+sudo usermod -aG docker $(whoami)
+cd ~
